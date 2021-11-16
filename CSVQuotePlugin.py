1cf3fe955075b16b87e56683bbb426707603377b
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-11-16 15:50:17 -0500

    Improved, checks if data is quoted already before quoting Also does not quote numerical data

--- CSVQuotePlugin.py
+++ CSVQuotePlugin.py
@@ -1,3 +1,12 @@
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def is_quoted(s):
+    return (s[0] == '\"' and s[len(s)-1] == '\"')
 
 class CSVQuotePlugin:
     def input(self, filename):
@@ -9,18 +18,30 @@ class CSVQuotePlugin:
     def output(self, filename):
         outfile = open(filename, 'w')
 
-        firstline = self.infile.readline()
-        contents = firstline.strip().split(',')
-        outfile.write(contents[0])
-        outfile.write(',')
-        for i in range(1, len(contents)):
-            outfile.write("\""+contents[i]+"\"")
-            if (i != len(contents)-1):
-                outfile.write(',')
-            else:
-                outfile.write('\n')
-        
         for line in self.infile:
-            outline = "\"" + line[:line.find(',')] + "\"" + line[line.find(','):]
-            outfile.write(outline)
+            contents = line.strip().split(',')
+            for i in range(len(contents)):
+                if (not is_number(contents[i]) and not is_quoted(contents[i])):
+                    contents[i] = '\"' + contents[i] + '\"'
+                outfile.write(contents[i])
+                if (i != len(contents)-1):
+                    outfile.write(',')
+                else:
+                    outfile.write('\n')
+        # Old version, just quoted header and first column
+        # New version above more flexible, checks if quoted already and if number
+        #firstline = self.infile.readline()
+        #contents = firstline.strip().split(',')
+        #outfile.write(contents[0])
+        #outfile.write(',')
+        #for i in range(1, len(contents)):
+        #    outfile.write("\""+contents[i]+"\"")
+        #    if (i != len(contents)-1):
+        #        outfile.write(',')
+        #    else:
+        #        outfile.write('\n')
+        #
+        #for line in self.infile:
+        #    outline = "\"" + line[:line.find(',')] + "\"" + line[line.find(','):]
+        #    outfile.write(outline)
 
